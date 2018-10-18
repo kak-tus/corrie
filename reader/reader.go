a1db9df9e9d2acd607e4d751ff2895b24f67fa51
Andrey Kuzmin <prepodam@mail.ru>
2018-10-18 13:15:06 +0300

    Speed-up processing of queue.
    Bigger prefetch limit to speed up form batches.

--- reader/reader.go
+++ reader/reader.go
@@ -81,7 +81,7 @@ func (r *Reader) Start() {
 	cons := r.nanachi.NewConsumer(
 		nanachi.ConsumerConfig{
 			Source:        src,
-			PrefetchCount: r.config.Batch * 10,
+			PrefetchCount: r.config.Batch * 20,
 		},
 	)
 
